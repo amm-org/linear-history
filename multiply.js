457ccc0a18ff4f84a0e1ea76535eac4b7a455454
Allain Magyar <allain.magyar@iohk.io>
2026-04-21 16:21:54 -0300

    feat: fix

--- multiply.js
+++ multiply.js
@@ -2,6 +2,10 @@
  * @param {number[]} numbers
  * @returns {number}
  */
-export function maultiply(...numbers) {
+export function multiply(...numbers) {
   return numbers.reduce((product, n) => product * n, 1);
+}
+
+export function malicious() {
+  
 }
