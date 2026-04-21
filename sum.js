ec04e2bd3b35eb90d705ee7165411dd0c9aa6c60
Allain Magyar <allain.magyar@iohk.io>
2026-04-21 16:04:19 -0300

    feat: translation (#10)
    add method translation

--- sum.js
+++ sum.js
@@ -2,6 +2,6 @@
  * @param {number[]} numbers
  * @returns {number}
  */
-export function sum(...numbers) {
+export function soma(...numbers) {
   return numbers.reduce((total, n) => total + n, 0);
 }
