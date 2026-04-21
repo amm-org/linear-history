b620ed05e2afdea35259e22f802425215f7a5090
Allain Magyar <allain.magyar@iohk.io>
2026-04-21 16:07:51 -0300

    feat: break

--- sum.js
+++ sum.js
@@ -2,6 +2,6 @@
  * @param {number[]} numbers
  * @returns {number}
  */
-export function sum(...numbers) {
+export function sumAll(...numbers) {
   return numbers.reduce((total, n) => total + n, 0);
 }
